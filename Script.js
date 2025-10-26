f969672385fde86928d18913a8567176d9cb2de3
john12gideon-cmyk <john12gideon@gmail.com>
2025-10-26 19:58:18 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,10 +1,3 @@
-// ======= Mobile Menu Toggle =======
-const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("nav-links");
-
-menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
 
 // ======= Smooth Scroll to Anchors =======
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {

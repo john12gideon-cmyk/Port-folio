189c02c43510dfcba5358c34761e56ce1103a8b7
john12gideon-cmyk <john12gideon@gmail.com>
2025-10-28 19:09:46 +0100

    Delete assests/js/dark-Mode-Toggle.js

--- assests/js/dark-Mode-Toggle.js
+++
@@ -1,6 +0,0 @@
-// ======= Dark Mode Toggle =======
-const darkToggle = document.querySelector(".dark-mode-toggle");
-darkToggle?.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-  localStorage.setItem("theme", document.body.classList.contains("dark-mode") ? "dark" : "light");
-});

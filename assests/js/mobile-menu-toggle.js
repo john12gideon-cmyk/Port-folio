0e2aa7c8b4d94cbee2759b4a2caef19f18537534
john12gideon-cmyk <john12gideon@gmail.com>
2025-10-28 19:09:31 +0100

    Delete assests/js/mobile-menu-toggle.js

--- assests/js/mobile-menu-toggle.js
+++
@@ -1,7 +0,0 @@
-/ ======= Mobile Menu Toggle =======
-const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("nav-links");
-
-menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
